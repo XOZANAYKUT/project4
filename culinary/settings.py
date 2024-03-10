53d5561b3f9b8e23591ba2dca60c81cebc74c72d
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-10 19:18:04 +0000

    add course and comment models

--- culinary/settings.py
+++ culinary/settings.py
@@ -90,6 +90,12 @@ DATABASES = {
 }
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
