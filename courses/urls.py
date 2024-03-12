21d4da473764e03f4ad9ec927763ee9fe36646c4
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-12 11:06:39 +0000

    cretate coursedetail.html and Combined and streamlined course-related URL configurations

--- courses/urls.py
+++ courses/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CourseList.as_view(), name='home'),
+    path('<slug:slug>/', views.course_detail, name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Course
 # Create your views here.
@@ -7,3 +7,26 @@ class CourseList(generic.ListView):
     queryset = Course.objects.filter(status=1)
     template_name = "courses/index.html"
     paginate_by = 6
+
+def course_detail(request, slug):
+    """
+    Display an individual :model:courses.Course.
+
+    *Context*
+
+    `course`
+        An instance of :model:courses.Course.
+
+    *Template:*
+
+    :template:courses/course_detail.html
+    """
+
+    queryset = Course.objects.filter(status=1)
+    course = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "courses/course_detail.html",
+        {"course": course},
+    )
