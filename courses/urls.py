a55290c857298236b55834d5a48d487bf6c8c3bf
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-15 17:20:35 +0000

    added comment CRUD

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,10 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.CourseList.as_view(), name='home'),
     path('<slug:slug>/', views.course_detail, name='course_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,30 +1,54 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
-from .models import Course
+from .models import Course, Comment
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .forms import CommentForm
-# Create your views here.
 
 class CourseList(generic.ListView):
     queryset = Course.objects.filter(status=1)
     template_name = "courses/index.html"
     paginate_by = 6
 
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Course.objects.filter(status=1)
+        course = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.course = course
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Course.objects.filter(status=1)
+    course = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('course_detail', args=[slug]))    
+
 def course_detail(request, slug):
-    """
-    Display an individual :model:courses.Course.
-
-    *Context*
-
-    `course`
-        An instance of :model:courses.Course.
-
-    *Template:*
-
-    :template:courses/course_detail.html
-    """
-
-    
     queryset = Course.objects.filter(status=1)
     course = get_object_or_404(queryset, slug=slug)
     comments = course.comments.all().order_by("-created_on")
@@ -39,7 +63,7 @@ def course_detail(request, slug):
             comment.course = course
             comment.save()
             messages.success(request, 'Comment submitted and awaiting approval')
-            return redirect('course_detail', slug=slug)  # Burada sayfayı tekrar yükleyerek formun temizlenmesini sağlıyoruz.
+            return redirect('course_detail', slug=slug)
 
     return render(
         request,
