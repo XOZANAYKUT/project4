2f1d6d662078e91d3747a1e9e94c005846924387
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-15 12:52:29 +0000

    Updated CommentForm to use correct model and fields

--- courses/forms.py
+++ courses/forms.py
@@ -3,5 +3,5 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = Comment
-        fields = ('body',)
+         model = CollaborateRequest
+         fields = ('name', 'email', 'message')
