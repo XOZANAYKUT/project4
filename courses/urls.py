0c921d369ebe84f7a9d966d850754ed6e6216eea
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-15 17:54:28 +0000

    added comment CRUD

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.CourseList.as_view(), name='home'),
     path('<slug:slug>/', views.course_detail, name='course_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
          views.comment_delete, name='comment_delete'),
 ]
