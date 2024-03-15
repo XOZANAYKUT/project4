b78434415b48522e531edda96145bf929c252021
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-15 15:39:49 +0000

    Updated form layout and added feedback

--- about/urls.py
+++ about/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.about_me, name='about'),
+    path('submit_collaborate_request/', views.submit_collaborate_request, name='submit_collaborate_request'),
 ]
