cd7e6d691623f204f2e30253faa4450b1c4b6cb2
XOZAN Aykut <hozanbesir21@gmail.com>
2024-03-21 15:02:44 +0000

    Improved display of comments, now showing comments made by all users

--- courses/forms.py
+++ courses/forms.py
@@ -5,5 +5,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+        
 class SearchForm(forms.Form):
     query = forms.CharField(label='Search')    
